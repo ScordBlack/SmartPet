1b10d547d6d083ad59587503b72834532227f143
ScordBlack <51029867+ScordBlack@users.noreply.github.com>
2021-08-10 11:40:53 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', 'localhost');
-define('USUARIO', 'id9609465_root');
-define('SENHA', '12345');
-define('DB', 'id9609465_login');
+define('HOST', ''); //nome do host
+define('USUARIO', ''); //nome do usuario
+define('SENHA', ''); //senha
+define('DB', ''); //nome do banco
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
